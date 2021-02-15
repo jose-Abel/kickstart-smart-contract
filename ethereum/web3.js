290352a3f4291d096895de269d46530ceb658229
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-15 13:45:00 -0400

    Update index.js to create the CampaignIndex class base component

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,15 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if(typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/f5675b420d8545948863cf7087b28a4e"
+  );
+
+  web3 = new Web3(provider);
+}
 
 export default web3;
