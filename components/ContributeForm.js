2038576bbb2235b097d5b1e76aa2d705f5eff4f8
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-16 14:00:16 -0400

    Update the ContributeForm component to include the onSubmit function

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
 import { Form, Input, Button, Message } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
 
 class ContributeForm extends Component {
+  state = {
+    value: "",
+    loading: false
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether") 
+      });
+
+    } catch(err) {
+
+    }
+  }
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input 
+            value={this.state.value}
+            onChange={event => this.setState({value: event.target.value})}
             label="ether"
-            labelPosition="right"/>
+            labelPosition="right"
+            />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+        <Button loading={this.state.loading} primary>Contribute!</Button>
       </Form>
     )
   }
