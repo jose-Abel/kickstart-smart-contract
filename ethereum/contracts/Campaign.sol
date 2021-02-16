d8d35dd7ab8e32429dcf4271784089f8afe9a813
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-16 12:44:16 -0400

    Update the Campaign.sol contract to add 2 methods, getSummary and getRequestsCount

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.17;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
-    
-    function createCampaign(uint minimum) public {
+
+    function createCampaign(uint256 minimum) public {
         address newCampaign = new Campaign(minimum, msg.sender);
-        
+
         deployedCampaigns.push(newCampaign);
     }
-    
+
     function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     }
@@ -17,10 +17,10 @@ contract CampaignFactory {
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address recipient;
         bool complete;
-        uint approvalCount;
+        uint256 approvalCount;
         mapping(address => bool) approvals;
     }
 
@@ -28,26 +28,30 @@ contract Campaign {
     address public manager;
     uint256 public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public approversCount;
+    uint256 public approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum, address creator) public {
+    function Campaign(uint256 minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        
+
         approvers[msg.sender] = true;
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(
+        string description,
+        uint256 value,
+        address recipient
+    ) public restricted {
         Request memory newRequest =
             Request({
                 description: description,
@@ -60,23 +64,47 @@ contract Campaign {
         requests.push(newRequest);
     }
 
-    function approveRequest(uint index) public {
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
-        
+
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
-        
+
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
-    
-    function finalizeRequest(uint index) public restricted {
+
+    function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
-        
+
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-        
+
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint256) {
+        return requests.length;
     }
 }
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x5fE27621b33bE8f235Db27709A99A3AeB1D7b852"
+  "0x3c81aE080F80260CFe5731bF768Df3742b233767"
 );
 
 export default instance;
