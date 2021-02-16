9c7ed43aa2be60df220c8b4846b1b60719bf2b0f
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-16 17:53:45 -0400

    Update ContributeForm.js to add validation to the input for contribute button

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { Form, Input, Button, Message } from "semantic-ui-react";
 import Campaign from "../ethereum/campaign";
 import web3 from "../ethereum/web3";
+import { Router } from "../routes";
 
 class ContributeForm extends Component {
   state = {
     value: "",
+    errorMessage: "",
     loading: false
   }
 
@@ -14,22 +16,28 @@ class ContributeForm extends Component {
 
     const campaign = Campaign(this.props.address);
 
+    this.setState({loading: true, errorMessage: ""});
+
     try {
       const accounts = await web3.eth.getAccounts();
 
-      campaign.methods.contribute().send({
+      await campaign.methods.contribute().send({
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, "ether") 
       });
 
-    } catch(err) {
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
 
+    } catch(err) {
+      this.setState({errorMessage: err.message});
     }
+
+    this.setState({loading: false, value: ""});
   }
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input 
@@ -39,6 +47,9 @@ class ContributeForm extends Component {
             labelPosition="right"
             />
         </Form.Field>
+
+        <Message error header="Oops!" content={this.state.errorMessage} />
+
         <Button loading={this.state.loading} primary>Contribute!</Button>
       </Form>
     )
