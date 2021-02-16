6300b597cb85fb56c7ce37d6626b7cf90f27bf40
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-16 18:03:44 -0400

    Create index.js for campaigns/requests and set up basic structure

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "../../../components/Layout";
+
+class RequestIndex extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3>Request List</h3>
+      </Layout>
+    )
+  }
+}
+
+export default RequestIndex;
