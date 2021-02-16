31ed09c7c8a675ffa7d7617bcb12e39f4ab825ef
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-16 11:25:01 -0400

    Update the campaign/new component to use the Router object from routes file to redirect to homepage after user creates a campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,8 @@ import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+import { Router } from "../../routes";
+
 
 class CampaignNew extends Component {
   state = {
@@ -21,7 +23,9 @@ class CampaignNew extends Component {
 
       await factory.methods.createCampaign(this.state.minimumContribution).send({
         from: accounts[0]
-      })
+      });
+
+      Router.pushRoute("/");
     }
     catch(err) {
       this.setState({errorMessage: err.message});
